1c891cf49278fcf7e6fd1e2c593596e87a154f3a
Shivam Savani <shivambsavani@gmail.com>
2020-12-24 00:13:32 +1030

    Initial Commit

---
+++ Idea/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Idea
+//
+//  Created by Shivam Savani on 24/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Idea/IdeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaApp.swift
+//  Idea
+//
+//  Created by Shivam Savani on 24/12/20.
+//
+
+import SwiftUI
+
+@main
+struct IdeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
